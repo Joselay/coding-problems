45f61c8f2dcfb6c79f76df6dae09f9ac073aefef
Smae Tongmenglay <98274892+Joselay@users.noreply.github.com>
2022-11-11 07:30:33 +0700

    Create A-Spell Check.java

---
+++ Codeforces/A-Spell Check.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+//https://codeforces.com/contest/1722/problem/A
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            String str = sc.nextLine();
+            char temp[] = str.toCharArray();
+            Arrays.sort(temp);
+            String res = new String(temp);
+            System.out.println(res.equals("Timru") ? "YES":"NO");
+        }
+    }
+}
