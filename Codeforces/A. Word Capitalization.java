4c52119e274a87f7a7d28849790f4895243a9280
Smae Tongmenglay <98274892+Joselay@users.noreply.github.com>
2022-11-11 08:24:17 +0700

    Create A. Word Capitalization.java

---
+++ Codeforces/A. Word Capitalization.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+//https://codeforces.com/problemset/problem/281/A
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                if (st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+
+        String str = sc.next();
+        StringBuilder res = new StringBuilder(str);
+        char ans = Character.toUpperCase(str.charAt(0));
+        res.setCharAt(0,ans);
+        System.out.println(res);
+    }
+}
