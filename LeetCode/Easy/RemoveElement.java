22261aaf674a29e452b2311a55df4b0c39672bff
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-10 21:08:23 +0700

    Create RemoveElement.java

---
+++ LeetCode/Easy/RemoveElement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    int i = 0;
+    int j = 0;
+    while (i < nums.length) {
+        if (nums[i] != val) {
+            nums[j] = nums[i];
+            j++;
+        }
+        i++;
+    }
+    return j;
+}
+
+}
