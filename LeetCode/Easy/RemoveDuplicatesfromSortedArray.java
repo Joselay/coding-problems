ea9d54f54446e6061624e61e8c16811bcfe87c60
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-10 22:53:28 +0700

    Update RemoveDuplicatesfromSortedArray.java

--- LeetCode/Easy/RemoveDuplicatesfromSortedArray.java
+++ LeetCode/Easy/RemoveDuplicatesfromSortedArray.java
@@ -1,15 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-   public int removeDuplicates(int[] nums) {
-    if (nums.length == 0) return 0;
-    int currentNonDuplicate = nums[0];
-    int uniqueCount = 1;
-    for (int i = 1; i < nums.length; i++)
-        if (nums[i] != currentNonDuplicate) {
-            nums[uniqueCount] = nums[i];
-            currentNonDuplicate = nums[i];
-            uniqueCount++;
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode curr = head;
+        while(curr!=null && curr.next!=null){
+            if(curr.val == curr.next.val)
+                curr.next = curr.next.next;
+            else
+                curr = curr.next;
         }
-
-    return uniqueCount;
+        return head;
     }
 }
