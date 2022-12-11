33ec3bf209cd4e5b19f5ea021d926db374a82aa7
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-11 12:31:35 +0700

    Create Pascal'sTriangleII.java

---
+++ LeetCode/Easy/Pascal'sTriangleII.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+            row.add(1);
+        }
+
+        return row;
+    }
+}
