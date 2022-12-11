7338266098cb6490ef65bc67d956fb41dffc9ce3
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-11 10:42:34 +0700

    Update JumpingOnTheClouds.cpp

--- HackerRank/JumpingOnTheClouds.cpp
+++ HackerRank/JumpingOnTheClouds.cpp
@@ -1,29 +1,20 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    int n, k;
-    cin >> n >> k;
 
-    vector<int> c(n);
-    for (int i = 0; i < n; i++)
-        cin >> c[i];
-
-    int energy = 100;
-    int pos = 0;
-    do
-    {
-        pos = (pos + k) % n;
-        energy--;
-        if (c[pos] == 1)
-            energy -= 2;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> c(n, INT_MAX/2);
+    for(int i = 0;i < n;i++){
+       int x;
+        cin >> x;
+        if (x == 1) continue;
+        if (i == 0) c[i] = 0;
+        else if (i == 1) c[i] = 1 + c[0];
+        else c[i] = min(c[i-1], c[i-2]) + 1;
     }
-    while (pos != 0);
-
-    cout << energy << endl;
-
+    cout << c.back() << endl;
     return 0;
 }
