72fa2425afd82b538a10a2d459cd1e8b88459a6b
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-11 12:47:51 +0700

    Create BinaryTreeMaximumPathSum.java

---
+++ LeetCode/Hard/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    public int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewPath = node.val + leftGain + rightGain;
+
+        maxSum = Math.max(maxSum, priceNewPath);
+
+
+        return node.val + Math.max(leftGain, rightGain);
+    }
+}
