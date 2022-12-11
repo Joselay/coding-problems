18e75d034800f6223d9e7fb3cf27e016387e8062
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-11 13:15:36 +0700

    Create ReverseNodesInK-Group.java

---
+++ LeetCode/Hard/ReverseNodesInK-Group.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+    int count = 0;
+    while (curr != null && count != k) {
+        curr = curr.next;
+        count++;
+    }
+    if (count == k) {
+        curr = reverseKGroup(curr, k);
+        while (count-- > 0) {
+            ListNode temp = head.next;
+            head.next = curr;
+            curr = head;
+            head = temp;
+        }
+        head = curr;
+    }
+    return head;
+    }
+}
