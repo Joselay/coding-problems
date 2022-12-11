3b65db6a245bf9de6dcbfbeaa9679dcf63f9b109
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-11 17:20:59 +0700

    Create 3SumClosest.java

---
+++ LeetCode/Medium/3SumClosest.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int closestSum = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum;
+                }
+            }
+        }
+
+        return closestSum;
+    }
+}
