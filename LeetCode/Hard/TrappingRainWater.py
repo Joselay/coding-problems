a89c0bbab0034b777245c9037992b2427e7b38d7
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-11 13:41:08 +0700

    Create TrappingRainWater.py

---
+++ LeetCode/Hard/TrappingRainWater.py
@@ -0,0 +1,25 @@
+class Solution:
+    def trap(self, height):
+        if not height:
+            return 0
+        
+        n = len(height)
+        left_max = [0] * n
+        right_max = [0] * n
+        
+        # fill the left_max array
+        left_max[0] = height[0]
+        for i in range(1, n):
+            left_max[i] = max(height[i], left_max[i-1])
+            
+        # fill the right_max array
+        right_max[n-1] = height[n-1]
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(height[i], right_max[i+1])
+            
+        # calculate the trapped water at each index
+        water = 0
+        for i in range(n):
+            water += min(left_max[i], right_max[i]) - height[i]
+            
+        return water
