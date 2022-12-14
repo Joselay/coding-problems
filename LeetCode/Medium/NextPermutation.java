97a145e4b237eab8014d23e886c5a4a21300a2e5
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-14 08:19:57 +0700

    Create NextPermutation.java

---
+++ LeetCode/Medium/NextPermutation.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+}
