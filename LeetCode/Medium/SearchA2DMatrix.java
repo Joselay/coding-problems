bb9c7efc40a694bdde91346aea774cc9e4595706
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-21 19:02:51 +0700

    Create SearchA2DMatrix.java

---
+++ LeetCode/Medium/SearchA2DMatrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = m * n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / n][mid % n];
+            if (midVal == target) return true;
+            else if (midVal < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
