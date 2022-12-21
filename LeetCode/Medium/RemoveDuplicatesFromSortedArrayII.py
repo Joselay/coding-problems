8df538f5e40ced8b9e25b302cca01cebfc6c2cd0
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-21 20:05:50 +0700

    Create RemoveDuplicatesFromSortedArrayII.py

---
+++ LeetCode/Medium/RemoveDuplicatesFromSortedArrayII.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+            elif i == 0 or nums[i-1] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+        
+        return i + 1
