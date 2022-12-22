66416cfbb8923c1f77eb7835dd03d876b702e5f5
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-22 09:22:01 +0700

    Create FlattenBinaryTreeToLinkedList.java

---
+++ LeetCode/Medium/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = left;
+        while (root.right != null) {
+            root = root.right;
+        }
+        root.right = right;
+    }
+}
