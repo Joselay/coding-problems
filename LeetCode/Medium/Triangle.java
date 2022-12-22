134e6b93399d7cf2ffee788056660939a86cdf59
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-22 09:26:15 +0700

    Create Triangle.java

---
+++ LeetCode/Medium/Triangle.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+}
