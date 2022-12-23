73e78c1f8f7d8361b8ddf1ecaea314a6f612b85e
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-23 13:13:48 +0700

    Create CopyListWithRandomPointer.java

---
+++ LeetCode/Medium/CopyListWithRandomPointer.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Map<Node, Node> map = new HashMap<>();
+        Node curr = head;
+        while (curr != null) {
+            map.put(curr, new Node(curr.val, null, null));
+            curr = curr.next;
+        }
+
+        curr = head;
+        while (curr != null) {
+            map.get(curr).next = map.get(curr.next);
+            map.get(curr).random = map.get(curr.random);
+            curr = curr.next;
+        }
+
+        return map.get(head);
+    }
+}
