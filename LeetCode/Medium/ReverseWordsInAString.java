c576ed8f305010cefb5a773c44791f1e64117706
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-25 14:08:54 +0700

    Create ReverseWordsInAString.java

---
+++ LeetCode/Medium/ReverseWordsInAString.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
