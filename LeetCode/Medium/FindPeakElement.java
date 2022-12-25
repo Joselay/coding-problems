5152db00a7fb40bb7bcc758278dc2ec4c2d76bde
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-25 14:13:25 +0700

    Create FindPeakElement.java

---
+++ LeetCode/Medium/FindPeakElement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
