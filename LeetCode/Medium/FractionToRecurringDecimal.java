66669121bff1dba96c379459d29d36399a85e4fa
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-25 14:14:56 +0700

    Create FractionToRecurringDecimal.java

---
+++ LeetCode/Medium/FractionToRecurringDecimal.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder res = new StringBuilder();
+        if ((numerator < 0) ^ (denominator < 0)) {
+            res.append("-");
+        }
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        res.append(num / den);
+        num %= den;
+        if (num == 0) {
+            return res.toString();
+        }
+        res.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(num, res.length());
+        while (num != 0) {
+            num *= 10;
+            res.append(num / den);
+            num %= den;
+            if (map.containsKey(num)) {
+                int index = map.get(num);
+                res.insert(index, "(");
+                res.append(")");
+                break;
+            } else {
+                map.put(num, res.length());
+            }
+        }
+        return res.toString();
+    }
+}
