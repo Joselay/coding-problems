8545ddf09e892a104c40df922157cfc8a3fc648b
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-27 14:40:58 +0700

    Create LowestCommonAncestorOfABinaryTree.js

---
+++ LeetCode/Medium/LowestCommonAncestorOfABinaryTree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (!root || root === p || root === q) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (!left) return right;
+    if (!right) return left;
+    return root;
+};
