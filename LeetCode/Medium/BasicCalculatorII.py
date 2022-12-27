9f54d9d1b7224ee5af3d02d7ab3c6859ce252a33
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-27 14:32:38 +0700

    Create BasicCalculatorII.py

---
+++ LeetCode/Medium/BasicCalculatorII.py
@@ -0,0 +1,20 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        num = 0
+        sign = '+'
+        for i in range(len(s)):
+            if s[i].isdigit():
+                num = num * 10 + int(s[i])
+            if s[i] in '+-*/' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack[-1] = stack[-1] * num
+                elif sign == '/':
+                    stack[-1] = int(stack[-1] / num)
+                sign = s[i]
+                num = 0
+        return sum(stack)
