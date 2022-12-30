85fc816daa6978b319460c06d95377eff6351672
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2022-12-30 08:27:48 +0700

    Update GrayCode.cpp

--- CSES/GrayCode.cpp
+++ CSES/GrayCode.cpp
@@ -8,19 +8,20 @@ int main () {
 	cin.tie(0);
 
 
-	int n; cin>>n;
-    int a[1<<n + 1] = {0};
-    for (int i = 0; i < n; i++)
-    	cout<<0;
-    cout<<endl;
-    a[0] = 1;
-    int x = 0;
-    for (int i = 1; i < 1<<n; i++) {
-    	for (int j = 0; j < n; j++) {
-    		int y = x^(1<<j);
-    		if (!a[y]) {
-    			x = y;
-    			a[y] = 1;
+	int n;
+	cin>>n;
+	int a[1<<n + 1] = {0};
+	for (int i = 0; i < n; i++)
+		cout<<0;
+	cout<<endl;
+	a[0] = 1;
+	int x = 0;
+	for (int i = 1; i < 1<<n; i++) {
+		for (int j = 0; j < n; j++) {
+			int y = x^(1<<j);
+			if (!a[y]) {
+				x = y;
+				a[y] = 1;
 				string s;
 				while (y) {
 					if (y&1) s += '1';
@@ -30,11 +31,13 @@ int main () {
 				reverse(s.begin(), s.end());
 				for (int i = 0; i < n - s.size(); i++)
 					cout<<0;
+
 				cout<<s<<endl;
 				break;
-    		}
-    	}
-    }
+  
+			}
+		}
+	}
 
 	return 0;
 }
