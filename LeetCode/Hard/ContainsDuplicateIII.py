396ab408191e4ed41c578d7753b2be89aa158991
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2023-01-01 19:12:20 +0700

    Create ContainsDuplicateIII.py

---
+++ LeetCode/Hard/ContainsDuplicateIII.py
@@ -0,0 +1,19 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        if t < 0:
+            return False
+        n = len(nums)
+        d = {}
+        w = t + 1
+        for i in range(n):
+            m = nums[i] // w
+            if m in d:
+                return True
+            if m - 1 in d and abs(nums[i] - d[m - 1]) < w:
+                return True
+            if m + 1 in d and abs(nums[i] - d[m + 1]) < w:
+                return True
+            d[m] = nums[i]
+            if i >= k:
+                del d[nums[i - k] // w]
+        return False
