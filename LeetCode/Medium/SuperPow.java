e92c7af509135d93f4caaed05cbd65b3688d23ae
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2023-01-01 07:25:55 +0700

    Create SuperPow.java

---
+++ LeetCode/Medium/SuperPow.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int superPow(int a, int[] b) {
+        if (a % 1337 == 0) return 0;
+        int p = 0;
+        for (int i : b) p = (p * 10 + i) % 1140;
+        if (p == 0) p += 1440;
+        return power(a, p, 1337);
+    }
+
+    private int power(int a, int b, int mod) {
+        a %= mod;
+        int res = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) res = res * a % mod;
+            a = a * a % mod;
+            b >>= 1;
+        }
+        return res;
+    }
+}
