279ce0d447de2d83420ad30242615ce663f342d2
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2023-01-01 07:32:30 +0700

    Create LinkedListRandomNode.java

---
+++ LeetCode/Medium/LinkedListRandomNode.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode head;
+    private Random rand;
+    public Solution(ListNode head) {
+        this.head = head;
+        rand = new Random();
+    }
+    public int getRandom() {
+        ListNode c = head;
+        int r = c.val;
+        for (int i = 1; c.next != null; i++) {
+            c = c.next;
+            if (rand.nextInt(i + 1) == i) r = c.val;
+        }
+        return r;
+    }
+}
