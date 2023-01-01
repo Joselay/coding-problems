35937b76d18165f9a27436fd2263ae8a33aba261
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2023-01-01 07:37:22 +0700

    Create LongestAbsoluteFilePath.java

---
+++ LeetCode/Medium/LongestAbsoluteFilePath.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthLongestPath(String input) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int maxLen = 0;
+        for (String s : input.split("\n")) {
+            int lev = s.lastIndexOf("\t") + 1;
+            while (lev + 1 < stack.size()) stack.pop();
+            int len = stack.peek() + s.length() - lev + 1;
+            stack.push(len);
+            if (s.contains(".")) maxLen = Math.max(maxLen, len - 1); 
+        }
+        return maxLen;
+    }
+}
