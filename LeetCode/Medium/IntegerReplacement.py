a89cb22485e51f60c3b0b1d073cbf9f891caeb40
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2023-01-03 15:46:22 +0700

    Create IntegerReplacement.py

---
+++ LeetCode/Medium/IntegerReplacement.py
@@ -0,0 +1,7 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        if n == 1:
+            return 0
+        if n % 2 == 0:
+            return self.integerReplacement(n // 2) + 1
+        return min(self.integerReplacement(n - 1), self.integerReplacement(n + 1)) + 1
