b5c1765c467d6def12cd35218afbafbcb58ee5d5
Smae Tongmenglay <tongmenglaysmae5@gmail.com>
2023-02-26 18:58:14 +0700

    Create HappyNumber.py

---
+++ LeetCode/Easy/HappyNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n = sum(int(digit)**2 for digit in str(n))
+        return True
